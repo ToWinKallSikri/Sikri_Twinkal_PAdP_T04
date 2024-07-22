d42ba22759a8ba8ae6155c9ed9aa622e73f788cd
ToWinKallSikri <116014231+ToWinKallSikri@users.noreply.github.com>
2024-07-22 14:40:37 +0200

    Update

--- Traccia_04_Sikri_Twinkal/Extensions/MiddlewareExtension.cs
+++ Traccia_04_Sikri_Twinkal/Extensions/MiddlewareExtension.cs
@@ -1,6 +1,50 @@
-﻿namespace Traccia_04_Sikri_Twinkal.Web.Extensions
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+using Traccia_04_Sikri_Twinkal.App.Factories;
+
+namespace Traccia_04_Sikri_Twinkal.Web.Extensions
 {
-    public class MiddlewareExtension
+    public static class MiddlewareExtensions
     {
+        public static WebApplication? AddWebMiddleware(this WebApplication? app)
+        {
+            
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.Use(async (HttpContext context, Func<Task> next) =>
+            {
+                //await context.Response.WriteAsync("Prova");
+                await next.Invoke();
+            });
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        var res = ResponseFactory
+                            .WithError(contextFeature.Error);
+                        await context.Response.WriteAsJsonAsync(
+                            res
+                            );
+                    }
+                });
+            });
+
+            app.MapControllers();
+            return app;
+        }
     }
 }
--- Traccia_04_Sikri_Twinkal/Results/BadRequestResultFactory.cs
+++ Traccia_04_Sikri_Twinkal/Results/BadRequestResultFactory.cs
@@ -1,6 +1,25 @@
-﻿namespace Traccia_04_Sikri_Twinkal.Web.Results
+﻿using Microsoft.AspNetCore.Mvc;
+using Traccia_04_Sikri_Twinkal.App.Models.Responses;
+
+
+namespace Traccia_04_Sikri_Twinkal.Web.Results
 {
-    public class BadResponseFactory
+    public class BadRequestResultFactory : BadRequestObjectResult
     {
+        public BadRequestResultFactory(ActionContext context) : base(new BadResponse())
+        {
+            var retErrors = new List<string>();
+            foreach (var key in context.ModelState)
+            {
+                var errors = key.Value.Errors;
+                for (var i = 0; i < errors.Count(); i++)
+                {
+                    retErrors.Add(errors[0].ErrorMessage);
+                }
+            }
+
+            var response = (BadResponse)Value;
+            response.Errors = retErrors;
+        }
     }
 }
