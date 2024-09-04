45c7638e7cadf659f086c5e46ace3aa9bcd5e8f5
twinfore <116014231+ToWinKallSikri@users.noreply.github.com>
2024-09-04 15:50:35 +0200

    Final

--- Traccia_04_Sikri_Twinkal.App/Models/Requests/GetUtenteRequest.cs
+++ Traccia_04_Sikri_Twinkal.App/Models/Requests/GetUtenteRequest.cs
@@ -5,10 +5,7 @@
         public int PageSize { get; set; }
         public int PageNumber { get; set; }
         public int? UtenteId { get; set; }
-<<<<<<< HEAD
         public string Name { get; set; } = string.Empty;
-=======
-        public string Name { get; set; }
->>>>>>> d0cde68f64c7570638764fa080443da3a7e68971
+
     }
 }
